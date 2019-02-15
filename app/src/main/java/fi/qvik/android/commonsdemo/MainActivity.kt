b222c8a964f619fe427e2cf055faa4b82d119e41
Ville Valta <ville.valta@gmail.com>
2019-02-15 11:16:07 +0200

    initial commit

---
+++ app/src/main/java/fi/qvik/android/commonsdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package fi.qvik.android.commonsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
