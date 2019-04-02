321b3ae15d19a1e8230b5f88b66d9f344c910536
Ville Valta <ville.valta@gmail.com>
2019-04-02 12:49:33 +0300

    use library in the demo

--- app/src/main/java/fi/qvik/android/commonsdemo/MainActivity.kt
+++ app/src/main/java/fi/qvik/android/commonsdemo/MainActivity.kt
@@ -1,12 +1,18 @@
 package fi.qvik.android.commonsdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import fi.qvik.android.commons.utils.underline
+import fi.qvik.android.commonsdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.hello.underline(true)
     }
 }
